33fe08771c1b28440f2646474604653d92aa2510
chawonlee <ss01066865089@gmail.com>
2021-06-01 17:32:19 +0900

    인터넷 권한 추가

--- app/src/main/java/com/cookandriod/window_remote/web_open.java
+++ app/src/main/java/com/cookandriod/window_remote/web_open.java
@@ -8,22 +8,20 @@ import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import static com.cookandriod.window_remote.MainActivity.texr;
+
 
 public class web_open extends AppCompatActivity {
 
     private WebView mWebView;
     private WebSettings mWebSettings;
-    public static String texr;
-    private EditText my_ip;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.web_view);
 
-        //아이피 입력 에디트텍스트
-        my_ip = (EditText) findViewById(R.id.my_ip);
-        texr=my_ip.getText().toString();
 
         mWebView = (WebView)findViewById(R.id.web_view);
         mWebView.setWebViewClient(new WebViewClient());
